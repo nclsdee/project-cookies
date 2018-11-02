e08bac657cdb2d46957becf83660d4430d8bc4ee
Darlene Graterol <dcgb93@gmail.com>
2018-11-02 02:36:06 -0700

    display map

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,13 +5,24 @@ class ItemsController < ApplicationController
     if params[:query].present?
       @items = Item.where(category: params[:category])
       @items_sorted = @items.near(params[:query], params[:radius].to_i)
-      raise
     else
       @items = Item.all
     end
-  end
 
+
+  @items = Item.where.not(latitude: nil, longitude: nil)
+
+   @markers = @items.map do |item|
+      {
+      lat: item.latitude,
+      lng: item.longitude,
+        # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
+      }
+  end
+  raise
+end
   def show
     @item = Item.find(params[:id])
   end
 end
+
