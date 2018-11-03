8a19338858e1f0ed587ea3010437818e2c969b48
Nico Dee <nicolas.dee@whu.edu>
2018-11-03 15:30:40 +0100

    photo loading, flashes for reservation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,5 @@
 import "bootstrap";
+import "../plugins/flatpickr"
 import GMaps from 'gmaps/gmaps.js';
 
 import { autocomplete } from "../components/autocomplete.js";
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  validates :title, :description, :price, presence: true
+  validates :title, :description, :price, :category, presence: true
   belongs_to :user
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
